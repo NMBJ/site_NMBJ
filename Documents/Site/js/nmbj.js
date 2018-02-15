4abab50418fe69fbee4f5c0a7c94540163a654ec
FelixDelaunay <felix-delaunay@hotmail.fr>
2018-02-15 17:31:17 +0100

    Changement police

--- Documents/Site/js/nmbj.js
+++ Documents/Site/js/nmbj.js
@@ -1,6 +1,21 @@
 function afficher(asElement,asImg)
 {
+	//Modification de l'image
 	$(asElement).attr("src","images/"+asImg + ".jpg");
+	changeText(asElement);
+}
+
+function changeText(asElement){
+	var texte = "none";
+	var nom = "Who is he?";
+	if(asElement == "#jb_pres"){ texte = "Cet amateur de galette saucisses..."; nom = "JB"}
+	if(asElement == "#adrien_pres") {texte = "Ce grand charmeur ...."; nom = "Adrilll"}
+	if(asElement == "#coco_pres") {texte = "Il a tartiné, il a pilé"; nom = "CoCo"}
+	if(asElement == "#fefe_pres") {texte = "Ce tchatcheur en puissance..."; nom = "Feyfey"}
+	if(asElement == "#pierre_pres") {texte = "Le bel aviateur avec sa moustache bien taillée"; nom = "Pierro"}
+	$("#texte_pres").text(texte);
+	$("#prenom_pres").text(nom);
+	
 }
 
 $(document).ready(function()
