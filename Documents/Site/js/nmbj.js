c1e6b22f11e04e6a72107ee3370966ffd5f93d19
FelixDelaunay <felix-delaunay@hotmail.fr>
2018-02-22 17:57:03 +0100

    Ajout vidéos, finalisation sonothèque

--- Documents/Site/js/nmbj.js
+++ Documents/Site/js/nmbj.js
@@ -1,12 +1,17 @@
 this.suiteNote = [];
 this.score = 0;
+this.song = "Sentinelle";
 var play = false;
+
+me = this;
+
 function afficher(asElement,asImg)
 {
 	//Modification de l'image
 	$(asElement).attr("src","images/"+asImg + ".jpg");
 	
 }
+
 
 function changeText(asElement,event){
 	var texte = "";
@@ -25,7 +30,7 @@ function changeText(asElement,event){
 		son = "sol";
 	}
 	if(asElement == "#adrien_pres") {
-		texte = "Ce grand charmeur ....";
+		texte = "Notre bassiste en puissance ";
 		nom = "Adrilll";
 		citation = '"ça va gros malin?"';
 		image = "basse";
@@ -41,7 +46,7 @@ function changeText(asElement,event){
 		son = "si";
 	}
 	if(asElement == "#fefe_pres") {
-		texte = "Ce tchatcheur en puissance...";
+		texte = "Entre chant et guitare, ce ligérien à la voix exceptionnelle, ambiance la scène comme personne.";
 		nom = "Feyfey";
 		citation = '"Une salle, une ambiance, une sale ambiance!"';
 		image = "guitare";
@@ -93,6 +98,16 @@ function changeText(asElement,event){
 
 }
 
+//Jouer la vidéo de présentation
+function playVideo(){
+
+	//On affiche la vidéo
+	document.getElementById("overlay1").style.display = "block";
+
+	//On joue la vidéo
+	document.querySelector('#video_pres').play();
+}
+
 //Lancer le son de la page web
 function playSound(son){
 	//On change le son 
@@ -108,7 +123,9 @@ function playMusique(son){
 		//On joue le son
 		document.querySelector('#sound').play();
 		//Animation : Photo qui tourne dans le sens inverse des aiguilles d'une montre
-		$("#Vinyl").addClass("play-vinyl");
+		$("#Vinyl").addClass("rotate-vinyl");
+		$("#play_vinyl").removeClass("stop-vinyl");
+		$("#play_vinyl").addClass("play-vinyl");
 		play = true;
 	}
 	else
@@ -116,10 +133,13 @@ function playMusique(son){
 		//on arrete le son
 		document.querySelector('#sound').pause();
 		play = false;
-		$("#Vinyl").removeClass("play-vinyl");
+		$("#Vinyl").removeClass("rotate-vinyl");
+		$("#play_vinyl").removeClass("play-vinyl");
+		$("#play_vinyl").addClass("stop-vinyl");
 	}	
 }
 
+//Jeux caché
 function gameSound(note){
 	//On ajoute la note au tableau
 	this.suiteNote.push(note);
@@ -128,10 +148,10 @@ function gameSound(note){
 	if(this.suiteNote.join() == "sol,sol,sol,la,si,la,sol,si,la,la,sol"){
 
 		//On affiche la vidéo
-		document.getElementById("overlay").style.display = "block";
+		document.getElementById("overlay2").style.display = "block";
 
 		//On joue la vidéo
-		document.querySelector('#video_palmashow').play();
+		document.querySelector('#video_bonus').play();
 	}
 	
  	//A chaque clic, on annule le timeout
@@ -145,6 +165,21 @@ function gameSound(note){
 
 $(document).ready(function()
 {
+
+	//Démarragge de la vidéo si on click sur "Watch the video"
+	$("#playVideo").click(function(){
+		playVideo();		
+	});
+
+	//On stop la vidéo si on click sur elle
+	$("#overlay1").click(function(){
+		document.getElementById("overlay1").style.display = "none";
+		
+		//On stop la vidéo
+		document.querySelector('#video_pres').pause();
+		document.querySelector('#video_pres').currentTime = 0;		
+	});
+
 	$("#jb_pres").mouseover(function() { afficher("#jb_pres","jb"); changeText("#jb_pres","mouseover");});
 	$("#jb_pres").mouseout(function() { afficher("#jb_pres","jb_nb"); changeText("#jb_pres","mouseout");});
 
@@ -178,26 +213,79 @@ $(document).ready(function()
 		gameSound(son);
 	});
 	$("#sentinelle").click(function(){
-		son = "Sentinelle";
-		playMusique(son);
+		me.son = "Sentinelle";
+		changeTitleSound(me.son);
+	});
+	$("#505Room").click(function(){
+		me.son = "505room";
+		changeTitleSound(me.son);		
+	});
+	$("#DemainDeBonMatin").click(function(){
+		me.son = "DemainDeBonMatin";
+		changeTitleSound(me.son);		
 	});
 
-	//Quitter la vidéo du jeu
-	$("#overlay").click(function(){
-		document.getElementById("overlay").style.display = "none";
+	//Quitter la vidéo du jeu caché
+	$("#overlay2").click(function(){
+		document.getElementById("overlay2").style.display = "none";
 		
 		//On stop la vidéo
-		document.querySelector('#video_palmashow').pause();
-		document.querySelector('#video_palmashow').currentTime = 0;
+		document.querySelector('#video_bonus').pause();
+		document.querySelector('#video_bonus').currentTime = 0;
 	});
 
+	//Gérer le click sur le vinyl
+	$("#play_vinyl").click(function(){
+		playMusique(me.son)
+	});
 
-	for(var i=0; i<10; i++){
+	//Démarrer le jeux des bulles
+	newGame(1);
+
+	var hauteur = document.getElementById("sonotheque").offsetHeight;
+	$('#Vinyl').width(hauteur*0.7);
+	$('#play_vinyl').height($('#Vinyl').width());
+} );
+
+//Changer le titre sélectionné
+function changeTitleSound(son){
+	$("#sentinelle").addClass("text-title-not-selectionned");
+	$("#505Room").addClass("text-title-not-selectionned");
+	$("#DemainDeBonMatin").addClass("text-title-not-selectionned");
+	$("#sentinelle").removeClass("text-title-selectionned");
+	$("#505Room").removeClass("text-title-selectionned");
+	$("#DemainDeBonMatin").removeClass("text-title-selectionned");
+
+	if(son == "Sentinelle"){
+		$("#sentinelle").removeClass("text-title-not-selectionned");
+		$("#sentinelle").addClass("text-title-selectionned");		
+	} 
+	if(son == "505room"){
+		$("#505Room").removeClass("text-title-not-selectionned");
+		$("#505Room").addClass("text-title-selectionned");
+	} 
+	if(son == "DemainDeBonMatin"){
+		$("#DemainDeBonMatin").removeClass("text-title-not-selectionned");
+		$("#DemainDeBonMatin").addClass("text-title-selectionned");
+	} 
+}
+
+//Nouvelle partie pour le jeux
+function newGame(niveau){
+
+	var tailleEcran = window.innerWidth;	
+
+	for(var i=0; i<10*niveau; i++){
 		
+		//Instanciation de l'image
 		var image = new Image();
 		image.src = "images/bulle.png";	
 		image.height = (Math.random())*20+50;
-		image.style = "animation-delay:"+10*Math.random()+"s;left:"+(Math.random())*1400+"px; position:absolute; z-index:2;";
+
+		//Positionner les bulles sur l'écran
+		var position = (Math.random())*(tailleEcran-2*image.height) + image.height;
+
+		image.style = "animation-delay:"+10*Math.random()+"s;left:"+position+"px; position:absolute; z-index:2;";
 		image.id = "bubble"+i;
 		$("#containerBubble").append(image);
 		$('#bubble'+i).attr('class', 'animation-bubbles'); 
@@ -207,11 +295,29 @@ $(document).ready(function()
 			this.remove();
 			//On actualise le score
 			setScore(getScore()+1);
+
+			//Si toutes les bulles ont été éclatée
+			if(getScore() == 10*niveau){
+				//On démarre avec le niveau suivant
+				if (niveau < 3) newGame(niveau+1);
+				//Si on gagne après le niveau 3, on a gagné la partie
+				else{
+					gameWin();
+					return true;
+				} 
+				setScore(0);
+			} 
+
 			//On affiche le score
 			$('#score').text(getScore());
 		}); 
 	}	
-} );
+}
+
+function gameWin(){
+	$('#score').text("Il n'y a plus de bulles dans le jacuzzi...");
+	$('#score').addClass("game-win");
+}
 
 function setScore(score){
 	this.score = score;
